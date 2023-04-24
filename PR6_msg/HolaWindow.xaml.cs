6edc15b164951fc85de6e5492845c6a484fee5cb
pihta <pihta@PIHTA4>
2023-04-24 14:09:26 +0300

    Add project files.

---
+++ PR6_msg/HolaWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Wpf.Ui.Appearance;
+
+namespace PR6_msg
+{
+    /// <summary>
+    /// Interaction logic for HolaWindow.xaml
+    /// </summary>
+    public partial class HolaWindow : Window
+    {
+        public HolaWindow()
+        {
+            InitializeComponent();
+            Theme.Apply(
+            ThemeType.Dark,         // Theme type
+            BackgroundType.Mica, // Background type
+            true                                      // Whether to change accents automatically
+            );
+
+            Theme.Apply(
+            ThemeType.Dark,         // Theme type
+            BackgroundType.Mica, // Background type
+            true                                      // Whether to change accents automatically
+            );
+
+
+            Loaded += (sender, args) =>
+            {
+                Wpf.Ui.Appearance.Watcher.Watch(
+                    this,                                  // Window class
+                    Wpf.Ui.Appearance.BackgroundType.Mica, // Background type
+                    true,                                   // Whether to change accents automatically
+                    true
+                );
+            };
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        public void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            
+        }
+        private void callmsgr()
+        {
+            MainWindow newWindow = new MainWindow();
+            newWindow.Show();
+            this.Close();
+        }
+    }
+}
---
+++ PR6_msg/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Wpf.Ui.Appearance;
+using Wpf.Ui.Controls;
+
+namespace PR6_msg
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<string> messages = new List<string>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            HolaWindow newWindow = new HolaWindow();
+            newWindow.Show();
+            Theme.Apply(
+            ThemeType.Light,         // Theme type
+            BackgroundType.Mica, // Background type
+            true                                      // Whether to change accents automatically
+            );
+
+            Theme.Apply(
+            ThemeType.Dark,         // Theme type
+            BackgroundType.Mica, // Background type
+            true                                      // Whether to change accents automatically
+            );
+
+
+            Loaded += (sender, args) =>
+            {
+                Wpf.Ui.Appearance.Watcher.Watch(
+                    this,                                  // Window class
+                    Wpf.Ui.Appearance.BackgroundType.Mica, // Background type
+                    true,                                   // Whether to change accents automatically
+                    true
+                );
+            };
+        }
+
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            sendmessage();
+        }
+
+        public void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                sendmessage();
+            }
+        }
+        private void sendmessage()
+        {
+            if (!string.IsNullOrEmpty(MessageInput.Text))
+            {
+                messages.Add(MessageInput.Text);
+                MessageInput.Text = string.Empty;
+            }
+            MessageList.ItemsSource = messages;
+        }
+
+    }
+}
